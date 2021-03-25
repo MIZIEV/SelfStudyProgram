19ba1af041eb046a0a74e906cf6afb3a930b8fd0
MizZzev <mizzzev@outlook.com>
2021-03-25 23:49:06 +0200

    - refactor OutputText/ResultMessage

--- src/main/java/View/OutputText.java
+++ src/main/java/View/OutputText.java
@@ -4,10 +4,15 @@ import javafx.scene.control.TextArea;
 
 public class OutputText extends TextArea {
 
+    private static final int XCORDINATES = 168;
+    private static final int YCORDINATES = 50;
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 700;
+
     public OutputText() {
-        this.setLayoutX(168);
-        this.setLayoutY(50);
-        this.setPrefSize(700, 700);
+        this.setLayoutX(XCORDINATES);
+        this.setLayoutY(YCORDINATES);
+        this.setPrefSize(WIDTH, HEIGHT);
         this.setEditable(false);
     }
 }
--- src/main/java/View/ResultMessage.java
+++ src/main/java/View/ResultMessage.java
@@ -4,10 +4,15 @@ import javafx.scene.control.*;
 
 public class ResultMessage extends TextArea {
 
-    public ResultMessage(){
-        this.setLayoutX(885);
-        this.setLayoutY(50);
-        this.setPrefSize(135, 300);
+    private static final int XCORDINATES = 885;
+    private static final int YCORDINATES = 50;
+    private static final int WIDTH = 135;
+    private static final int HEIGHT = 300;
+
+    public ResultMessage() {
+        this.setLayoutX(XCORDINATES);
+        this.setLayoutY(YCORDINATES);
+        this.setPrefSize(WIDTH, HEIGHT);
         this.setEditable(false);
     }
 }
