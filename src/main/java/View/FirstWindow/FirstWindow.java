941f4ab5d9ee732a959d8ee54654824d473c159f
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-05-10 10:39:46 +0300

    - add constants

--- src/main/java/View/FirstWindow/FirstWindow.java
+++ src/main/java/View/FirstWindow/FirstWindow.java
@@ -6,6 +6,7 @@ import View.SecondWindow.SecondWindow;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -15,6 +16,16 @@ public class FirstWindow {
     private SecondWindow secondWindow;
     private final Stage firstWindow = new Stage();
     private final GeneralDependence generalDependence;
+
+    private final static short PREFERRED_DECORATE_PANEL_HEIGHT = 150;
+    private final static short PREFERRED_DECORATE_PANEL_WIDTH = 200;
+    private final static short PREFERRED_HBOX_HEIGHT = 150;
+    private final static short PREFERRED_HBOX_WIDTH = 300;
+    private final static short PREFERRED_WINDOW_WIDTH = 800;
+    private final static short PREFERRED_WINDOW_HEIGHT = 600;
+    private final static short MIN_WINDOW_WIDTH = 650;
+    private final static short MIN_WINDOW_HEIGHT = 450;
+    private final static byte INTERVAL = 10;
 
     public FirstWindow(GeneralDependence genDependence) {
         this.generalDependence = genDependence;
@@ -42,23 +53,23 @@ public class FirstWindow {
         });
         String stylesheet = getClass().getResource("/FirstWindowStyle.css").toExternalForm();
 
-        BorderPane generalPane = new BorderPane();
+        BorderPane generalPane = new BorderPane();            // create general pane
 
-        BorderPane insertTopPane = new BorderPane();
-        insertTopPane.setPrefSize(200, 150);
-        Label title = new Label("Self learning program");
+        BorderPane insertTopPane = new BorderPane();          // set decorate top & bottom panel with title
+        insertTopPane.setPrefSize(PREFERRED_DECORATE_PANEL_WIDTH, PREFERRED_DECORATE_PANEL_HEIGHT);
+        Label title = new Label("Self study program");
         insertTopPane.setCenter(title);
         insertTopPane.getStyleClass().add("top-border-pane");
 
         BorderPane insertBottomPane = new BorderPane();
-        insertBottomPane.setPrefSize(200, 150);
+        insertBottomPane.setPrefSize(PREFERRED_DECORATE_PANEL_WIDTH, PREFERRED_DECORATE_PANEL_HEIGHT);
         insertBottomPane.setCenter(firstWinConstructor.getStartLearningButton());
         insertBottomPane.getStyleClass().add("bottom-border-pane");
 
-        HBox leftHBox = new HBox(10);
-        VBox firstLiftVBox = new VBox(10);
-        VBox secondLeftVBox = new VBox(10);
-        leftHBox.setPrefSize(300,150);
+        HBox leftHBox = new HBox(INTERVAL);                  //set left box with buttons
+        VBox firstLiftVBox = new VBox(INTERVAL);
+        VBox secondLeftVBox = new VBox(INTERVAL);
+        leftHBox.setPrefSize(PREFERRED_HBOX_WIDTH, PREFERRED_HBOX_HEIGHT);
         leftHBox.setAlignment(Pos.CENTER);
         firstLiftVBox.setAlignment(Pos.CENTER);
         secondLeftVBox.setAlignment(Pos.CENTER);
@@ -71,21 +82,21 @@ public class FirstWindow {
 
         leftHBox.getChildren().addAll(firstLiftVBox, secondLeftVBox);
 
-        HBox rightHBox = new HBox(10);
-        VBox firstRightVBox = new VBox(10);
-        VBox secondRightVBox = new VBox(10);
-        rightHBox.setPrefSize(300,150);
+        HBox rightHBox = new HBox(INTERVAL);                        //set right box with buttons
+        VBox firstRightVBox = new VBox(INTERVAL);
+        VBox secondRightVBox = new VBox(INTERVAL);
+        rightHBox.setPrefSize(PREFERRED_HBOX_WIDTH, PREFERRED_HBOX_HEIGHT);
         rightHBox.setAlignment(Pos.CENTER);
         firstRightVBox.setAlignment(Pos.CENTER);
         secondRightVBox.setAlignment(Pos.CENTER);
 
         firstRightVBox.getChildren().addAll(firstWinConstructor.getJava8Button(),
-                firstWinConstructor.getDataBaseButton(),firstWinConstructor.getJDBCButton());
+                firstWinConstructor.getDataBaseButton(), firstWinConstructor.getJDBCButton());
 
         secondRightVBox.getChildren().addAll(firstWinConstructor.getIOandNIOButton(),
-                firstWinConstructor.getSQLButton(),firstWinConstructor.getXMLButton());
+                firstWinConstructor.getSQLButton(), firstWinConstructor.getXMLButton());
 
-        rightHBox.getChildren().addAll(firstRightVBox,secondRightVBox);
+        rightHBox.getChildren().addAll(firstRightVBox, secondRightVBox);
 
         generalPane.setTop(insertTopPane);
         generalPane.setBottom(insertBottomPane);
@@ -94,10 +105,13 @@ public class FirstWindow {
         generalPane.setLeft(leftHBox);
         generalPane.setRight(rightHBox);
 
-        Scene firstWindowScene = new Scene(generalPane,800,600);
+        Scene firstWindowScene = new Scene(generalPane, PREFERRED_WINDOW_WIDTH, PREFERRED_WINDOW_HEIGHT);
         firstWindowScene.getStylesheets().add(stylesheet);
+        firstWindow.setMinWidth(MIN_WINDOW_WIDTH);
+        firstWindow.setMinHeight(MIN_WINDOW_HEIGHT);
         firstWindow.setScene(firstWindowScene);
-        firstWindow.setTitle("Start");
+        firstWindow.setTitle("Self study program");
+        firstWindow.getIcons().add(new Image("icon.jpg"));
         firstWindow.show();
     }
 }
