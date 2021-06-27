b678c460d372f0d3f5756c7df528302dfd5631ff
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-06-27 18:38:24 +0300

    - renamed class

---
+++ src/main/java/Model/ListIndex.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class ListIndex {
+
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
