00fd9cd332d262fb974f6c6cb688db8785cc5d40
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-07-08 18:23:36 +0300

    - renamed class & added description - added new method ( double countingFromFile(ArrayList<InfoFromDB> list))

---
+++ src/main/java/Model/ResultCounter.java
@@ -0,0 +1,45 @@
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *Class description:
+ * This class is needed to counting result of your learning, each flagged question will be marked and counted.
+ *
+ * Method description:
+ * double counting(); - this method count result during learning;
+ * double countingFromFile(ArrayList<InfoFromDB> list); - this method take ArrayList from saved files
+ * and count result from them.
+ */
+public class ResultCounter {
+
+    private final DBWorker worker;
+    private final ListIndex listIndex;
+    private double totalResult;
+
+    public ResultCounter(DBWorker work, ListIndex count) {
+        this.listIndex = count;
+        this.worker = work;
+    }
+
+    public double counting() {
+
+        if (!worker.getBufferList().get(listIndex.getIndex()).getYesNo()) {
+            double addend = 100.0 / worker.getBufferList().size();
+            totalResult += addend / 100.0;
+        }
+        return totalResult;
+    }
+
+    public double countingFromFile(ArrayList<InfoFromDB> list) {
+
+        double addend = 100.0 / list.size();
+
+        for (InfoFromDB element : list) {
+            if (element.getYesNo()) {
+                totalResult += addend / 100.0;
+            }
+        }
+        return totalResult;
+    }
+}
