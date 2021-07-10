fb2fe2a166f13ad7728678d13bdb8b0244cf7cfa
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-07-10 17:23:41 +0300

    - added new classes with getters

--- src/main/java/View/GeneralDependence.java
+++ src/main/java/View/GeneralDependence.java
@@ -3,11 +3,21 @@ package View;
 import Model.*;
 
 public class GeneralDependence {
-    private final DBConnector connector = new DBConnector();
-    private final DBWorker worker = new DBWorker(connector);
-    private final ListIndex listIndex = new ListIndex();
 
-    public DBWorker getWorker() { return worker; }
+    private final DBConnector connector = new DBConnector();
+    private final DBWorker DBWorker = new DBWorker(connector);
+    private final ListIndex listIndex = new ListIndex();
+    private final ResultCounter resultCounter = new ResultCounter(DBWorker, listIndex);
+    private final FileWorker fileWorker = new FileWorker();
+    private final JSONWorker jsonWorker = new JSONWorker(resultCounter, DBWorker);
+
+    public FileWorker getFileWorker() { return fileWorker; }
+
+    public ResultCounter getCountingResult() { return resultCounter; }
+
+    public JSONWorker getJsonWorker() { return jsonWorker; }
+
+    public DBWorker getDBWorker() { return DBWorker; }
 
     public ListIndex getListIndex() { return listIndex; }
 }
