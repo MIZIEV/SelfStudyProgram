8fe1f650e626d5924f94912311621e46c8083d44
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-07-11 13:23:40 +0300

    - renamed constants - added new constant - replaced some code

--- src/main/java/View/ErrorWindow.java
+++ src/main/java/View/ErrorWindow.java
@@ -15,26 +15,28 @@ public class ErrorWindow {
     private final static short WIN_HEIGHT = 300;
     private final static byte VBOX_SPACING = 20;
 
-    private final static String WIN_ICON = "/error_icon.jpg";
-    private final static String LABEL_STYLE = "Label";
-    private final static String PANE_STYLE = "Border-Pane";
+    private final static String STYLE_FILE = "/Styles/ErrorWindowStyle.css";
     private final static String WIN_TITLE = "Error window";
+    private final static String WIN_ICON = "/error_icon.jpg";
+    private final static String LABEL_SC = "Label";
+    private final static String PANE_SC = "Border-Pane";
 
-    public void launchWin(String errorText,String fixAdvise) {
-        String stylesheet = getClass().getResource("/ErrorWindowStyle.css").toExternalForm();
+    public void launchWin(String errorText, String adviceForFix) {
+        String stylesheet = getClass().getResource(STYLE_FILE).toExternalForm();
 
         Label errorMessage = new Label(errorText);
-        errorMessage.getStyleClass().add(LABEL_STYLE);
-        Label fixAdvice = new Label(fixAdvise);
-        fixAdvice.getStyleClass().add(LABEL_STYLE);
+        errorMessage.getStyleClass().add(LABEL_SC);
+        Label fixAdvice = new Label(adviceForFix);
+        fixAdvice.getStyleClass().add(LABEL_SC);
 
-        Stage window = new Stage();
         BorderPane mainPane = new BorderPane();
-        mainPane.getStyleClass().add(PANE_STYLE);
+        mainPane.getStyleClass().add(PANE_SC);
         VBox centralVBox = new VBox(VBOX_SPACING);
         centralVBox.setAlignment(Pos.CENTER);
-        centralVBox.getChildren().addAll(errorMessage,fixAdvice);
+        centralVBox.getChildren().addAll(errorMessage, fixAdvice);
         mainPane.setCenter(centralVBox);
+
+        Stage window = new Stage();
         Scene winScene = new Scene(mainPane, WIN_WIDTH, WIN_HEIGHT);
         winScene.getStylesheets().add(stylesheet);
         window.setTitle(WIN_TITLE);
