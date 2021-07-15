d9a8f1e71cbfacb89c3797127e9fbc72c9ba8246
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-07-15 22:59:27 +0300

    - replaced saving function from JSONWorker to FileWorker - added class description

--- src/main/java/Model/FileWorker.java
+++ src/main/java/Model/FileWorker.java
@@ -1,16 +1,25 @@
 package Model;
 
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
 
 /**
  * Class description:
  * This class is needed to getting all .txt files with saved information about last learning.
- *
+ * <p>
  * Methods description:
  * void searchFiles(); - this method search files in /resources/SavedFiles and then it put file's name to ArrayList;
  * File getFile(String fileName); - this method takes String with file name and return file with this name;
- * void removeFile(String fileName); - this method takes String with file name and remove this file.
+ * void removeFile(String fileName); - this method takes String with file name and remove this file;
+ * void putInfoToFile(ArrayList<InfoFromDB> list, ResultCounter result, JSONObject jsonObject); - this method
+ * takes JSON object and put they to file, also it takes ArrayList for taked learning theme and result for
+ * creating file name.
  */
 public class FileWorker {
 
@@ -18,7 +27,7 @@ public class FileWorker {
 
     private final static String SAVING_FILE_PATH = "C:\\IT\\Java\\Self study program\\src\\main\\Resources\\SavedFiles\\";
 
-    public void searchFiles() {
+    public final void searchFiles() {
         File searchFile = new File(SAVING_FILE_PATH);
         String savingFiles[] = searchFile.list();
 
@@ -28,14 +37,43 @@ public class FileWorker {
         }
     }
 
-    public File getFile(String fileName) {
+    public final File getFile(String fileName) {
         File savedFile = new File(SAVING_FILE_PATH + fileName);
         return savedFile;
     }
 
-    public void removeFile(String fileName) {
+    public final void removeFile(String fileName) {
         File fileToRemove = new File(SAVING_FILE_PATH + fileName);
         fileToRemove.delete();
+    }
+
+    public void putInfoToFile(ArrayList<InfoFromDB> list, ResultCounter result, JSONObject jsonObject) {
+        GregorianCalendar calendar = new GregorianCalendar();
+        String nowTime = calendar.getTime().toString();
+
+        HashSet<String> selectedTheme = new HashSet<>();
+        for (InfoFromDB element : list) {
+            selectedTheme.add(element.getTheme());
+        }
+
+        String splitString[] = nowTime.split("\\s|\\:");
+        double resultPercent = result.counting() * 100;
+        String fileName = splitString[1] + " " + splitString[2] +
+                " " + splitString[3] + " " + splitString[4] +
+                " Theme" + selectedTheme +
+                " Result " + (int) resultPercent + "%";
+        String bufferString = jsonObject.toString();
+        byte buf[] = bufferString.getBytes();
+        try {
+            File saveFile = new File(SAVING_FILE_PATH + fileName + ".txt");
+            saveFile.createNewFile();
+            FileOutputStream writer = new FileOutputStream(saveFile);
+            for (int i = 0; i < buf.length; i++) {
+                writer.write(buf[i]);
+            }
+        } catch (IOException e) {
+            System.out.println("error");
+        }
     }
 
     public ArrayList<String> getFilesName() { return filesName; }
--- src/main/java/Model/JSONWorker.java
+++ src/main/java/Model/JSONWorker.java
@@ -4,59 +4,42 @@ import org.json.JSONObject;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 
+/**
+ * class description:
+ * this class needed to converting information to JSON format with method - convertToJSON(ArrayList<InfoFromDB> list);
+ * also converting information from JSON with method - convertFromJSON(File fileWithJSON);
+ */
 public class JSONWorker {
 
-    private final CountingResult result;
+    private final ResultCounter result;
+    private double totalResult;
 
-    public JSONWorker(CountingResult result) {
+    public JSONWorker(ResultCounter result) {
         this.result = result;
     }
 
-    public void convertToJSON(ArrayList<InfoFromDB> list) {
-        GregorianCalendar calendar = new GregorianCalendar();
-        String nowTime = calendar.getTime().toString();
-        String splitString[] = nowTime.split("\\s|\\:");
-        double resultPercent = result.counting() * 100;
-        String fileName = splitString[0] + " " + splitString[1] +
-                " Day " + splitString[2] +
-                " Time " + splitString[3] + " " + splitString[4] +
-                " Result " + (int) resultPercent;
+    public JSONObject convertToJSON(ArrayList<InfoFromDB> list) {
 
         JSONObject obj = new JSONObject();
         obj.put("list", list);
-        System.out.println(fileName);
-        String bufferString = obj.toString();
-        byte buf[] = bufferString.getBytes();
-        try {
-            File saveFile = new File("C:\\IT\\Java\\Self study program\\src\\main\\Resources\\" + fileName + ".txt");
-            saveFile.createNewFile();
-            FileOutputStream writer = new FileOutputStream(saveFile);
-            for (int i = 0; i < buf.length; i++) {
-                writer.write(buf[i]);
-            }
-            System.out.println("done !!!");
-        } catch (IOException e) {
-            //  e.printStackTrace();
-            System.out.println("error");
-        }
+
+        return obj;
     }
 
-    public ArrayList<InfoFromDB>  convertFromJSON() {
-        InfoFromDB infoFromJSON;
+    public ArrayList<InfoFromDB> convertFromJSON(File fileWithJSON) {
+        InfoFromDB dataFromJSON;
         ArrayList<InfoFromDB> bufferList = new ArrayList<>();
+        bufferList.clear();
         int c;
         StringBuffer stringBuffer = new StringBuffer();
 
         try {
-            FileReader reader =
-                    new FileReader("C:\\IT\\Java\\Self study program\\src\\main\\Resources\\Mon Jul Day 05 Time 20 37 Result 33.txt");
-            System.out.println("string buf");
+            FileReader reader = new FileReader(fileWithJSON);
+
             while ((c = reader.read()) != -1) {
                 stringBuffer.append((char) c);
             }
-            System.out.println(stringBuffer);
 
             if (!stringBuffer.toString().isEmpty()) {
 
@@ -66,23 +49,24 @@ public class JSONWorker {
                 int jsonSize = obj.getJSONArray("list").length();
 
                 while (counter <= jsonSize - 1) {
-                    infoFromJSON = new InfoFromDB(
+                    dataFromJSON = new InfoFromDB(
                             obj.getJSONArray("list").getJSONObject(counter).getInt("index"),
                             obj.getJSONArray("list").getJSONObject(counter).getString("theme"),
                             obj.getJSONArray("list").getJSONObject(counter).getString("question"),
                             obj.getJSONArray("list").getJSONObject(counter).getString("answer"),
-                            obj.getJSONArray("list").getJSONObject(counter).getBoolean("yesNo"));
+                            obj.getJSONArray("list").getJSONObject(counter).getInt("yesNo"));
 
-                    bufferList.add(infoFromJSON);
+                    bufferList.add(dataFromJSON);
                     counter++;
                 }
             }
-            System.out.println("list");
-            System.out.println(bufferList);
-            System.out.println("done");
+            totalResult = result.countingFromFile(bufferList);
         } catch (IOException e) {
             System.out.println("error");
         }
-         return bufferList;
+        return bufferList;
+    }
+    public double getTotalResult() {
+        return totalResult;
     }
 }
