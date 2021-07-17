2aeb1cd354d202f0585700fc7498749f66280ecc
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-07-17 20:26:32 +0300

    - added constants & icon

--- src/main/java/View/SaveWindow.java
+++ src/main/java/View/SaveWindow.java
@@ -4,6 +4,7 @@ import View.buttonsPatterns.ButtonsPattern;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -16,6 +17,18 @@ public class SaveWindow {
     private final static String SC_TOP_DECOR_PANE = "top-pane";
     private final static String SC_BOTTOM_PANE = "bottom-pane";
 
+    private final static String WINDOW_ICON = "/save_win_icon.png";
+    private final static String WINDOW_TITLE = "Saving result";
+
+    private final static short WINDOW_HEIGHT = 250;
+    private final static short WINDOW_WIDTH = 400;
+    private final static short MIN_WIN_HEIGHT = 230;
+    private final static short MIN_WIN_WIDTH = 300;
+
+    private final static byte DECOR_PANE_HEIGHT = 50;
+    private final static byte BUTTON_WIDTH = 80;
+    private final static byte BUTTON_HEIGHT = 40;
+
     private final Stage window = new Stage();
 
     public void initWindow() {
@@ -27,11 +40,11 @@ public class SaveWindow {
         Pane bottomDecorPane = new Pane();
         Label text = new Label("File was saved");
 
-        topDecorePane.setPrefHeight(50);
-        bottomDecorPane.setPrefHeight(50);
+        topDecorePane.setPrefHeight(DECOR_PANE_HEIGHT);
+        bottomDecorPane.setPrefHeight(DECOR_PANE_HEIGHT);
         topDecorePane.getStyleClass().add(SC_TOP_DECOR_PANE);
         bottomDecorPane.getStyleClass().add(SC_BOTTOM_PANE);
-        ButtonsPattern okButton = new ButtonsPattern(80, 40, "ok");
+        ButtonsPattern okButton = new ButtonsPattern(BUTTON_WIDTH, BUTTON_HEIGHT, "ok");
 
         okButton.setOnAction(event -> window.close());
         mainVBox.setAlignment(Pos.CENTER);
@@ -40,10 +53,16 @@ public class SaveWindow {
         mainPane.setTop(topDecorePane);
         mainPane.setBottom(bottomDecorPane);
         mainPane.setCenter(mainVBox);
-        Scene winScene = new Scene(mainPane, 200, 200);
+        Scene winScene = new Scene(mainPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        window.setMaxWidth(WINDOW_WIDTH);
+        window.setMaxHeight(WINDOW_HEIGHT);
+        window.setMinWidth(MIN_WIN_WIDTH);
+        window.setMinHeight(MIN_WIN_HEIGHT);
         winScene.getStylesheets().add(stylesheet);
         window.initModality(Modality.APPLICATION_MODAL);
         window.setScene(winScene);
+        window.setTitle(WINDOW_TITLE);
+        window.getIcons().add(new Image(WINDOW_ICON));
         window.show();
     }
 }
