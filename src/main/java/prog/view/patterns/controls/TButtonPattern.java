8caf08a38043a61aac3f8c49a7a12e03a41cbed7
Alexandr <52121473+MizZzev@users.noreply.github.com>
2021-08-04 16:51:08 +0300

    - removed interface

--- src/main/java/prog/view/patterns/controls/TButtonPattern.java
+++ src/main/java/prog/view/patterns/controls/TButtonPattern.java
@@ -2,7 +2,7 @@ package prog.view.patterns.controls;
 
 import javafx.scene.control.ToggleButton;
 
-public class TButtonPattern extends ToggleButton  implements TBInterface{
+public class TButtonPattern extends ToggleButton {
 
     public TButtonPattern(int width, int height, String name) {
         this.setText(name);
